47922e2d2d371e2a55368c5368c7ac038c1ef20a
awmleer <awmleer@sparker.xyz>
2017-02-11 22:38:29 +0800

    add tab badge

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { LearnPage } from '../learn/learn';
 import { AboutPage } from '../about/about';
 import {SettingsPage} from "../settings/settings";
+import {WordService} from "../../services/word.service";
 
 @Component({
     templateUrl: 'tabs.html'
@@ -14,7 +15,7 @@ export class TabsPage {
     tab2Root: any = AboutPage;
     tab3Root: any = SettingsPage;
 
-    constructor() {
+    constructor(public wordService:WordService) {
 
     }
 }

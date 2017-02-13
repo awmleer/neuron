ad800c1f9b08cce239840b6fc99faead985a441f
awmleer <awmleer@sparker.xyz>
2017-02-13 17:03:41 +0800

    setting - scale

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -3,13 +3,31 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-settings',
-  templateUrl: 'settings.html'
+    selector: 'page-settings',
+    templateUrl: 'settings.html'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
+
+    setting={
+        scale:3
+    };
+
+    scaleChange():void{
+        console.log(this.setting.scale);
+        let s:number;
+        switch(this.setting.scale){
+            case 1: s=0.9;break;
+            case 2: s=0.95;break;
+            case 3: s=1.0;break;
+            case 4: s=1.05;break;
+            case 5: s=1.1;break;
+            default: s=1.0;break;
+        }
+        (<HTMLMetaElement>document.getElementsByTagName('meta')['viewport']).content=`width=device-width, initial-scale=${s}, minimum-scale=${s}, maximum-scale=${s}, user-scalable=no`;
+    }
 
 }

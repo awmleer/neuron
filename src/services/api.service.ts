dd01ad4c3dd56d17773d83594eb71a0e7d7e9d17
awmleer <awmleer@sparker.xyz>
2018-06-28 17:40:47 +0800

    add infinite scroll

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -62,7 +62,6 @@ export class ApiService {
       [param: string]: any
     } = null,
   ): Promise<any> {
-    console.log(url);
     let request = this.http.get(CONST.apiUrl + url, {
       params: params,
       observe: 'response',
@@ -75,7 +74,6 @@ export class ApiService {
   }
 
   async post(url: string, body: object = null): Promise<any> {
-    console.log(url);
     let request = this.http.post(CONST.apiUrl + url, body, {
       observe: 'response',
       headers: {
